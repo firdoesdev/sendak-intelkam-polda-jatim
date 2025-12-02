7e06a67c10731e74408e92938e95388dbd59055a
Muhammad Firdaus <mbex71@gmail.com>
2025-12-03 04:39:46 +0700

    styling theme twitter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,8 +37,9 @@
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
+        
+        <link href="https://fonts.bunny.net/css?family=open-sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet" />
+        
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
