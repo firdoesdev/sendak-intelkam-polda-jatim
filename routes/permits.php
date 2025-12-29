f34369a59f0f4a9d0589395aeaed52283a901f9b
Muhammad Firdaus <firdoes.dev@gmail.com>
2025-12-29 14:01:56 +0700

    fix - renewals

--- routes/permits.php
+++ routes/permits.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\Permits\PermitRenewalController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('/permits', PermitController::class)->names('permits');
-    
     // Permit Renewals
-    Route::get('/permits/renewals', [PermitRenewalController::class, 'index'])->name('permits.renewals.index');
-    Route::post('/permits/renewals', [PermitRenewalController::class, 'store'])->name('permits.renewals.store');
-    Route::post('/permits/renewals/{id}/approve', [PermitRenewalController::class, 'approve'])->name('permits.renewals.approve');
-    Route::post('/permits/renewals/{id}/reject', [PermitRenewalController::class, 'reject'])->name('permits.renewals.reject');
+    Route::get('/permits-renewals', [PermitRenewalController::class, 'index'])->name('permits.renewals.index');
+    Route::post('/permits-renewals', [PermitRenewalController::class, 'store'])->name('permits.renewals.store');
+    Route::post('/permits-renewals/{id}/approve', [PermitRenewalController::class, 'approve'])->name('permits.renewals.approve');
+    Route::post('/permits-renewals/{id}/reject', [PermitRenewalController::class, 'reject'])->name('permits.renewals.reject');
+
+    Route::resource('/permits', PermitController::class)->names('permits');
 });
--- routes/weapons.php
+++ routes/weapons.php
@@ -9,7 +9,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // Weapon Management
     Route::resource('/weapons', WeaponController::class)
         ->names('weapons');
-    
+
     // Weapon Transfer Requests
     Route::prefix('/weapons/transfer-requests')->name('weapons.transfer-requests.')->group(function () {
         Route::get('/', [WeaponTransferController::class, 'index'])->name('index');
@@ -17,12 +17,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('/{id}/approve', [WeaponTransferController::class, 'approve'])->name('approve');
         Route::post('/{id}/reject', [WeaponTransferController::class, 'reject'])->name('reject');
     });
-    
-    // Permit Renewals
-    Route::prefix('/permits/renewals')->name('permits.renewals.')->group(function () {
-        Route::get('/', [PermitRenewalController::class, 'index'])->name('index');
-        Route::post('/', [PermitRenewalController::class, 'store'])->name('store');
-        Route::post('/{id}/approve', [PermitRenewalController::class, 'approve'])->name('approve');
-        Route::post('/{id}/reject', [PermitRenewalController::class, 'reject'])->name('reject');
-    });
+
+
 });
