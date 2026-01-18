1d92cb0ca15ec06eadd83dd080719fc5891d8930
Muhammad Firdaus <mbex71@gmail.com>
2026-01-18 16:37:02 +0700

    fix faker

--- database/factories/WeaponFactory.php
+++ database/factories/WeaponFactory.php
@@ -10,13 +10,13 @@ class WeaponFactory extends Factory
     public function definition(): array
     {
         return [
-            'code' => fake()->unique()->regexify('[A-Z]{3}-[0-9]{5}'),
-            'name' => fake()->word() . ' ' . fake()->randomElement(['Pistol', 'Rifle', 'Shotgun']),
-            'permit_type' => fake()->randomElement(['SENPI', 'SPORT', 'POLSUS']),
-            'weapon_type' => fake()->randomElement(['Pistol', 'Rifle', 'Shotgun']),
-            'serial_number' => fake()->unique()->regexify('[A-Z0-9]{10}'),
-            'manufacturer' => fake()->company(),
-            'caliber' => fake()->randomElement(['.22', '9mm', '.45', '5.56mm']),
+            'code' => $this->faker->unique()->regexify('[A-Z]{3}-[0-9]{5}'),
+            'name' => $this->faker->word() . ' ' . $this->faker->randomElement(['Pistol', 'Rifle', 'Shotgun']),
+            'permit_type' => $this->faker->randomElement(['SENPI', 'SPORT', 'POLSUS']),
+            'weapon_type' => $this->faker->randomElement(['Pistol', 'Rifle', 'Shotgun']),
+            'serial_number' => $this->faker->unique()->regexify('[A-Z0-9]{10}'),
+            'manufacturer' => $this->faker->company(),
+            'caliber' => $this->faker->randomElement(['.22', '9mm', '.45', '5.56mm']),
             'acquisition_date' => now(),
             'condition' => 'good',
             'status' => 'available',
