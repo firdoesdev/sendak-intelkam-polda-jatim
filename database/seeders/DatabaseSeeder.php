e436cfb9ec4ef77679641d254fc1e5bb29ab2071
Muhammad Firdaus <mbex71@gmail.com>
2026-01-18 17:07:38 +0700

    fix seeder weapon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
                 PoliceUnitSeeder::class,
                 WarehouseSeeder::class,
                 OrganizationSeeder::class,
-                WeaponSeeder::class
+                // WeaponSeeder::class
             ]);
         }
         
